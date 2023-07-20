3c8edbd28c0fbe3d88873fc47b72a4e2bf4008f6
keonoh00 <keonoh00@gmail.com>
2023-07-20 20:27:40 +0900

    Setup TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,18 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RandomModule } from './random/random.module';
 import { ApolloDriver } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule
+      .forRoot
+      // Options will be configured in env file
+      (),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
     }),
-    RandomModule,
     // GraphQLModule.forRoot() is same as below code in NestJS way
     /*
     cosnt server = new ApolloServer({
@@ -17,6 +21,7 @@ import { ApolloDriver } from '@nestjs/apollo';
       resolvers,
     })
     */
+    RandomModule,
   ],
   controllers: [],
   providers: [],
