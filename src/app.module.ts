f6304232155a879e3b19e3f5e21162c9d6dc7b70
keonoh00 <keonoh00@gmail.com>
2023-07-20 20:47:06 +0900

    Setup process env in NestJS way, and finalize typeORM setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,35 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { RandomModule } from './random/random.module';
 import { ApolloDriver } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule
-      .forRoot
-      // Options will be configured in env file
-      (),
+    ConfigModule.forRoot({
+      isGlobal: true, // Access config module from anywhere in the application
+      envFilePath:
+        process.env.NODE_ENV === 'prod'
+          ? '.env.prod'
+          : process.env.NODE_ENV === 'test'
+          ? '.env.test'
+          : '.env.dev',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true, // When typeORM connects to the database, is going to also migrate your db to current state of your module
+      logging: true,
+
+      // UNKNOWN OPTIONS
+      // entities: ['Post', 'Category'],
+      // subscribers: [],
+      // migrations: [],
+    }),
     GraphQLModule.forRoot({
       driver: ApolloDriver,
       autoSchemaFile: true,
