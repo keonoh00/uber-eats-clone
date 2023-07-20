a2eeb820d3894d613ff392da38e3a797942778aa
keonoh00 <keonoh00@gmail.com>
2023-07-20 17:02:11 +0900

    Make random resolver

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # uber-eats-clone-server
 
 This is a backend repository for Uber Eats Clone
+
+Following [CodeFirst](https://docs.nestjs.com/graphql/quick-start#code-first) approach,
+we will use [NestJS](https://nestjs.com/) and [TypeGraphQL](https://typegraphql.com/) to build GraphQL server.
+Code first approach means that we will define GraphQL schema using TypeScript classes and generate GraphQL schema from them.
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { RandomModule } from './random/random.module';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    RandomModule,
+    // GraphQLModule.forRoot() is same as below code in NestJS way
+    /*
+    cosnt server = new ApolloServer({
+      typeDefs,
+      resolvers,
+    })
+    */
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
